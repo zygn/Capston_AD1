9d6cbb295f051b6d66741cdfffffe51798396b23
YundoChoi <48561168+zygn@users.noreply.github.com>
2021-05-31 14:15:47 +0900

    remove files

--- .sh
+++
@@ -1,5 +0,0 @@
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.11.tar.gz
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/AdditionalMaps_0.9.11.tar.gz
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.11_RSS.tar.gz
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Windows/CARLA_0.9.11.zip
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Windows/AdditionalMaps_0.9.11.zip
