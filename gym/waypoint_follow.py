f14afe547bee22ec1d330c855cc67f234f5ebed4
Kangjin_Kim <kkj1250@gmail.com>
2021-05-31 18:21:22 +0900

    testing
    집에가서 해야딩~

--- gym/waypoint_follow.py
+++ gym/waypoint_follow.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     work = {'mass': 3.463388126201571, 'lf': 0.15597534362552312, 'tlad': 0.82461887897713965, 'vgain': 0.90338203837889}
     # with open('./obs_example/config_obs.yaml') as file:
-    with open('./shorts/I_conf.yaml') as file:
+    with open('./new_round/config.yaml') as file:
         conf_dict = yaml.load(file, Loader=yaml.FullLoader)
     conf = Namespace(**conf_dict)
     episode = 1
@@ -32,35 +32,37 @@ if __name__ == '__main__':
 
         while not done:
             desire_obs = list()
-            for i in range(1, len(temp_obs) - 1):
-                if (temp_obs[i] > 5. and temp_obs[i] < 15.) and (temp_obs[i-1] * 1.4 < temp_obs[i] or temp_obs[i-1] * 0.6 > temp_obs[i]):
-                    start_idx_temp = i
-                    end_idx_temp = i
-                    max_idx_temp = i
-                    i += 1
+            # for i in range(1, len(temp_obs) - 1):
+            #     if (temp_obs[i] > 5. and temp_obs[i] < 15.) and (temp_obs[i-1] * 1.4 < temp_obs[i] or temp_obs[i-1] * 0.6 > temp_obs[i]):
+            #         start_idx_temp = i
+            #         end_idx_temp = i
+            #         max_idx_temp = i
+            #         i += 1
                     
-                    while temp_obs[i] > 5. and temp_obs[i-1] * 1.1 > temp_obs[i] and temp_obs[i-1] * 0.9 < temp_obs[i] and (i+1 < len(temp_obs)):
-                        if temp_obs[i] > temp_obs[max_idx_temp]:
-                            max_idx_temp = i
-                        i += 1
+            #         while temp_obs[i] > 5. and temp_obs[i-1] * 1.1 > temp_obs[i] and temp_obs[i-1] * 0.9 < temp_obs[i] and (i+1 < len(temp_obs)):
+            #             if temp_obs[i] > temp_obs[max_idx_temp]:
+            #                 max_idx_temp = i
+            #             i += 1
                     
-                    end_idx_temp = i
+            #         end_idx_temp = i
 
-                    temp_obs_step = [0]*4
-                    temp_obs_step[0] = start_idx_temp
-                    temp_obs_step[1] = end_idx_temp
-                    temp_obs_step[2] = max_idx_temp
-                    temp_obs_step[3] = temp_obs[max_idx_temp]
+            #         temp_obs_step = [0]*4
+            #         temp_obs_step[0] = start_idx_temp
+            #         temp_obs_step[1] = end_idx_temp
+            #         temp_obs_step[2] = max_idx_temp
+            #         temp_obs_step[3] = temp_obs[max_idx_temp]
 
-                    desire_obs.append(temp_obs_step)
-                i += 1
+            #         desire_obs.append(temp_obs_step)
+            #     i += 1
             
-            print(desire_obs)
+            # print(desire_obs)
 
-            temp_obs = obs['scans'][0][360:720]
-
+            # temp_obs = obs['scans'][0][360:720]
 
             speed, steer = planner.plan(obs['poses_x'][0], obs['poses_y'][0], obs['poses_theta'][0], work['tlad'], work['vgain'])
+            speed = 5
+            # steer, speed = [0,10]
+            print(obs['poses_x'][0], obs['poses_y'][0])
             speeds.append(speed)
             action = np.array([[steer, speed]])
             obs, step_reward, done, info = env.step(np.array(action))
